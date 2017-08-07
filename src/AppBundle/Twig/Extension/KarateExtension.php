1e4de3bf830fe873b67f318bea58f0c2091f0e0f
Skiru <mateusz.koziol15@gmail.com>
2017-08-07 20:49:16 +0200

    Funkcja obcinająca długość newsów do okreslonej. Aby zmienić długość zmien w index.html.twig, parametr

--- src/AppBundle/Twig/Extension/KarateExtension.php
+++ src/AppBundle/Twig/Extension/KarateExtension.php
@@ -4,6 +4,8 @@ namespace AppBundle\Twig\Extension;
 
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Twig_Environment;
+use Twig_SimpleFilter;
+use Twig_SimpleFunction;
 
 class KarateExtension extends \Twig_Extension {
 
@@ -37,9 +39,17 @@ class KarateExtension extends \Twig_Extension {
     public function getFunctions()
     {
         return array(
-            new \Twig_SimpleFunction('print_categories_list',array($this,'printCategoriesList'), array('is_safe' => array('html'))),
-            new \Twig_SimpleFunction('print_tags_cloud',array($this,'tagsCloud'), array('is_safe' => array('html')))
 
+            new Twig_SimpleFunction('print_categories_list',array($this,'printCategoriesList'), array('is_safe' => array('html'))),
+            new Twig_SimpleFunction('print_tags_cloud',array($this,'tagsCloud'), array('is_safe' => array('html')))
+
+        );
+    }
+
+    public function getFilters()
+    {
+        return array(
+            new Twig_SimpleFilter('ab_shorten', array($this, 'shorten'), array('is_safe' => array('html')))
         );
     }
 
@@ -97,6 +107,15 @@ class KarateExtension extends \Twig_Extension {
         }
 
         return $tagsList;
+    }
+
+    public function shorten($text, $length = 200, $wrapTag = 'p') {
+        $text = strip_tags($text);
+        $text = substr($text, 0, $length).'[...]';
+        $openTag = "<{$wrapTag}>";
+        $closeTag = "</{$wrapTag}>";
+
+        return $openTag.$text.$closeTag;
     }
 
 
